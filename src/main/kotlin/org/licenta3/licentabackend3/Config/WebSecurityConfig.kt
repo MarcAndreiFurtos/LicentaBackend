fe350b7939bf9bfc38908dd5890c3ad3300f4ea7
marc <fmarcandrei@gmail.com>
2025-04-19 00:58:02 +0300

    bug fixing and added alot

---
+++ src/main/kotlin/org/licenta3/licentabackend3/Config/WebSecurityConfig.kt
@@ -0,0 +1,30 @@
+package org.licenta3.licentabackend3.Config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+
+
+@Configuration
+@EnableWebSecurity
+class WebSecurityConfig {
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain =
+        http
+            .csrf { it.disable() }
+            .authorizeHttpRequests {
+                it.requestMatchers(HttpMethod.OPTIONS).permitAll()
+                    .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/webjars/**").permitAll()
+                    .anyRequest().permitAll()
+                    .and()
+                    .csrf().disable()
+            }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .build()
+}
+
--- src/main/kotlin/org/licenta3/licentabackend3/Controller/CardController.kt
+++ src/main/kotlin/org/licenta3/licentabackend3/Controller/CardController.kt
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/cards")
+@CrossOrigin(origins = ["*"] )
 class CardController(private val cardService: CardService) {
 
     @PostMapping("/tokenize")
